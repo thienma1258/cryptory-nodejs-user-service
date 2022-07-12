44597e9be777db66182da25c3111e260c4203411
dong-pham-ts <dong.pham@trustingsocial.com>
2022-07-12 15:16:38 +0700

    fix: lint

--- routes.ts
+++ routes.ts
@@ -1,11 +1,11 @@
-import {Context, Router, send} from './deps.ts'
+import {_Context, Router, _send} from './deps.ts'
 import type {RouterContext, Application} from './deps.ts'
 import {log} from './middleware.ts'
 
 // deno-lint-ignore no-explicit-any
 const router: any = new Router()
 
-router.get('/', ({params, response}: RouterContext<string>) => {
+router.get('/', ({_params, response}: RouterContext<string>) => {
     log.debug('Serving hello world')
     response.body = 'Hello world!'
 })
